6695885bb07109eaaed4b57fbc3171a7230325f4
paulotijero <paulotijero@gmail.com>
2024-02-25 15:15:19 -0500

    feat: footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,3 +1,17 @@
+import logo from '../../assets/img/logo-rimac-v2.png';
+
 export default function Index() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="container mx-auto flex flex-col space-y-6 bg-gray-800 px-6 py-9 mt-16">
+      <a title="Rimac" className="self-center">
+        <img src={logo} alt="Rimac" />
+      </a>
+
+      <hr className="border-t-1 border-gray-700" />
+
+      <span className="text-sm text-white font-normal self-center">
+        © 2023 RIMAC Seguros y Reaseguros.
+      </span>
+    </footer>
+  );
 }
